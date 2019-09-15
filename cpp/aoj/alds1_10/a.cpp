6fa79eaff1106c8fb7ab61266d1fd486ed2b6609
digika.ota <ota@digika.jp>
2019-09-15 16:23:18 +0900

    alds_1_10(動的計画法）の完了１

--- cpp/aoj/alds1_10/a.cpp
+++ cpp/aoj/alds1_10/a.cpp
@@ -87,6 +87,93 @@ const double PI = acos(-1.0);
 // #define debug(x) cerr << #x << " = " << (x) << " (L" << __LINE__ << ")" \
 //                       << " " << __FILE__ << endl;
 
+class FibSolver
+{
+private:
+    vector<LL> &ans_holder;
+
+public:
+    FibSolver(vector<LL> &vec) : ans_holder(vec)
+    {
+        ans_holder[0] = 1; // f(1)
+        ans_holder[1] = 1; // f(2)
+    }
+
+    long long solve_reccursive(int n) const;
+
+    long long solve_memoize(int n);
+
+    long long solve_dp(int n) const;
+
+    long long solve_general(int n) const;
+};
+
+LL FibSolver::solve_reccursive(int n) const
+{
+    if (n == 0 || n == 1)
+        return 1;
+    else
+        return solve_reccursive(n - 1) + solve_reccursive(n - 2);
+}
+
+LL FibSolver::solve_memoize(int n)
+{
+    if (n > ans_holder.size())
+    {
+        cout << n << " >= " << ans_holder.size() << endl;
+        throw exception();
+    }
+
+    int idx = n;
+
+    if (ans_holder[idx] > 0)
+    {
+        return ans_holder[idx];
+    }
+    else
+    {
+        LL ans = solve_memoize(n - 1) + solve_memoize(n - 2);
+        ans_holder[idx] = ans;
+        return ans;
+    }
+}
+
+LL FibSolver::solve_dp(int n) const
+{
+    LL former1 = 1;
+    LL former2 = 1;
+    LL ans;
+
+    FOR(i, 2, n + 1)
+    {
+        ans = former1 + former2;
+        former1 = former2;
+        former2 = ans;
+    }
+
+    return ans;
+}
+
+const int MAX_ELEMENT = 100000;
+
 int main()
 {
+    vector<LL> holder(MAX_ELEMENT, -1);
+
+    FibSolver solver(holder);
+
+    int n;
+    cin >> n;
+    LL ans;
+    try
+    {
+        ans = solver.solve_dp(n);
+    }
+    catch (exception e)
+    {
+        cout << "Exception!!!!" << endl;
+        ans = -1;
+    }
+
+    cout << ans << endl;
 }
--- cpp/aoj/alds1_10/c.cpp
+++ cpp/aoj/alds1_10/c.cpp
@@ -87,6 +87,101 @@ const double PI = acos(-1.0);
 // #define debug(x) cerr << #x << " = " << (x) << " (L" << __LINE__ << ")" \
 //                       << " " << __FILE__ << endl;
 
+class LCS_Solver
+{
+    vector<VL> vl;
+    string &s1;
+    string &s2;
+
+public:
+    LCS_Solver(size_t s, string &s1, string &s2)
+        : vl(s, VL(s, -1)), s1(s1), s2(s2)
+    {
+        vl[0][0] = 0;
+        REP(i, s)
+        {
+            vl[0][i] = 0;
+            vl[i][0] = 0;
+        }
+    }
+
+    LL solve_memoize(int s1n, int s2m);
+    LL solve_dp();
+};
+
+LL LCS_Solver::solve_memoize(int s1n, int s2m)
+{
+    // cout << s1n << ", " << s2m << endl;
+    if (vl[s1n][s2m] >= 0)
+    {
+        return vl[s1n][s2m];
+    }
+    else
+    {
+        LL ans;
+        if (s1[s1n - 1] == s2[s2m - 1])
+        {
+            ans =
+                solve_memoize(s1n - 1, s2m - 1) + 1;
+            vl[s1n][s2m] = ans;
+            return ans;
+        }
+        else
+        {
+            ans = max(
+                solve_memoize(s1n - 1, s2m),
+                solve_memoize(s1n, s2m - 1));
+            vl[s1n][s2m] = ans;
+            return ans;
+        }
+    }
+}
+
+LL LCS_Solver::solve_dp()
+{
+    int n = s1.size();
+    int m = s2.size();
+
+    FOR(i, 0, n)
+    {
+        int I = i + 1;
+        FOR(j, 0, n)
+        {
+            int J = j + 1;
+            if (s1[i] == s2[j])
+            {
+                vl[I][J] =
+                    vl[I - 1][J - 1] + 1;
+            }
+            else
+            {
+                vl[I][J] =
+                    max(vl[I - 1][J],
+                        vl[I][J - 1]);
+            }
+        }
+    }
+
+    return vl[n][m];
+}
+
+const size_t MAX_SIZE = 1001;
+
 int main()
 {
+    int n;
+    cin >> n;
+
+    LL ans;
+    REP(_, n)
+    {
+        string s1, s2;
+        cin >> s1;
+        cin >> s2;
+        // cout << s1.size() << " " << s2.size() << endl;
+
+        ans = LCS_Solver(MAX_SIZE, s1, s2)
+                  .solve_dp();
+        cout << ans << endl;
+    }
 }
