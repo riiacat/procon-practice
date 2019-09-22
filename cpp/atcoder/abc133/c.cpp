589540e46a97a8f37911768c38dc1d1480f46ca7
digika.ota <ota@digika.jp>
2019-09-22 21:48:29 +0900

    abc133 c

---
+++ cpp/atcoder/abc133/c.cpp
@@ -0,0 +1,114 @@
+#include <iostream>
+
+//include
+//------------------------------------------
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <deque>
+#include <stack>
+#include <queue>
+#include <bitset>
+#include <tuple>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <cctype>
+#include <string>
+#include <cstring>
+#include <ctime>
+#include <climits>
+
+using namespace std;
+
+//conversion
+//------------------------------------------
+inline int toInt(string s)
+{
+    int v;
+    istringstream sin(s);
+    sin >> v;
+    return v;
+}
+template <class T>
+inline string toString(T x)
+{
+    ostringstream sout;
+    sout << x;
+    return sout.str();
+}
+
+//math
+//-------------------------------------------
+template <class T>
+inline T sqr(T x) { return x * x; }
+
+//typedef
+//------------------------------------------
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef vector<long long> VL;
+typedef vector<string> VS;
+typedef pair<int, int> PII;
+typedef long long LL;
+
+//container util
+//------------------------------------------
+#define ALL(a) (a).begin(), (a).end()
+#define RALL(a) (a).rbegin(), (a).rend()
+#define PB push_back
+#define MP make_pair
+#define SZ(a) int((a).size())
+#define EACH(i, c) for (auto i = (c).begin(); i != (c).end(); ++i)
+#define EXIST(s, e) ((s).find(e) != (s).end())
+#define SORT(c) sort((c).begin(), (c).end())
+
+//repetition
+//------------------------------------------
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define REP(i, n) FOR(i, 0, n)
+
+//constant
+//--------------------------------------------
+const double EPS = 1e-10;
+const double PI = acos(-1.0);
+
+//clear memory
+#define CLR(a) memset((a), 0, sizeof(a))
+
+//debug
+// #define dump(x) cerr << #x << " = " << (x) << endl;
+// #define debug(x) cerr << #x << " = " << (x) << " (L" << __LINE__ << ")" \
+//                       << " " << __FILE__ << endl;
+
+int main()
+{
+    long long L, R;
+
+    cin >> L >> R;
+
+    if (R - L + 1 >= 2019)
+    {
+        cout << 0 << endl;
+        return 0;
+    }
+
+    int res = INT_MAX;
+    for (LL i = L; i < R + 1; i++)
+    {
+        for (LL j = i + 1; j < R + 1; j++)
+        {
+            res = min(int((i * j) % 2019), res);
+        }
+    }
+
+    cout << res << endl;
+}
