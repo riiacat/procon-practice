8f80744aa5d6b172c8360bc233644d1ab732b95c
otatakuma <otatakuma0714@gmail.com>
2020-08-08 19:10:52 +0900

    rust-the-book ch12.2

--- rust/tutorials/the-book/ch12-io-cli-project/minigrep/src/main.rs
+++ rust/tutorials/the-book/ch12-io-cli-project/minigrep/src/main.rs
@@ -1,3 +1,16 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let query = &args[1];
+    let filename = &args[2];
+
+    println!("クエリ: {}", query);
+    println!("検索対象ファイル: {}", filename);
+
+    let contents =
+        fs::read_to_string(filename).expect("ファイルの読み込み中にエラーが発生しました。");
+
+    println!("ファイルコンテンツ：\n{}", contents);
 }
