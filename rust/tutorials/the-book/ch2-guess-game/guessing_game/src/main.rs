97bcba4f95875a6b6a46184d304fe91d6689f536
otatakuma <otatakuma0714@gmail.com>
2020-08-08 11:54:22 +0900

    rust-the-book ch2 最初のプログラムを作成

--- rust/tutorials/the-book/ch2-guess-game/guessing_game/src/main.rs
+++ rust/tutorials/the-book/ch2-guess-game/guessing_game/src/main.rs
@@ -1,3 +1,14 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("数字を推測してください");
+    println!("あなたの推測を入力してください");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("行の読み取りに失敗しました");
+
+    println!("あなたの推測は： {}", guess);
 }
