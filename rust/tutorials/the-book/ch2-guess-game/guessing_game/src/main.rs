b7eb9eb32f372b4ffb84185a5914ee0199df543f
otatakuma <otatakuma0714@gmail.com>
2020-08-08 12:11:41 +0900

    rust-the-book ch2を読了

--- rust/tutorials/the-book/ch2-guess-game/guessing_game/src/main.rs
+++ rust/tutorials/the-book/ch2-guess-game/guessing_game/src/main.rs
@@ -1,14 +1,39 @@
+extern crate rand;
+
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
     println!("数字を推測してください");
-    println!("あなたの推測を入力してください");
 
-    let mut guess = String::new();
+    let sec_number = rand::thread_rng().gen_range(1, 101);
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("行の読み取りに失敗しました");
+    loop {
+        println!("あなたの推測を入力してください");
 
-    println!("あなたの推測は： {}", guess);
+        // println!("シークレットナンバーは: {}", sec_number);
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("行の読み取りに失敗しました");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("あなたの推測は： {}", guess);
+
+        match guess.cmp(&sec_number) {
+            Ordering::Less => println!("小さすぎます！！"),
+            Ordering::Greater => println!("大きすぎる。。"),
+            Ordering::Equal => {
+                println!("正解！！");
+                break;
+            }
+        }
+    }
 }
