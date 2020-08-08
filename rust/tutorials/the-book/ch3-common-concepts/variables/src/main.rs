4c12d7869acf7a43322ed40c9f9f1b861884084d
otatakuma <otatakuma0714@gmail.com>
2020-08-08 12:24:25 +0900

    rust-the-book ch3を読了

--- rust/tutorials/the-book/ch3-common-concepts/variables/src/main.rs
+++ rust/tutorials/the-book/ch3-common-concepts/variables/src/main.rs
@@ -1,3 +1,31 @@
 fn main() {
-    println!("Hello, world!");
+    // println!("Hello, world!");
+
+    let spaces = "    ";
+    let spaces = spaces.len();
+    println!("spaces: {}", spaces);
+
+    //3.3
+    another_fun(13);
+
+    println!(
+        "こんな形でも{{}}の最後の値が帰ってきます: {}",
+        {
+            let x = 3;
+            x + 1
+        }
+    );
+
+    //3.5
+    let a = [123, 234, 345, 456, 567];
+    for v in a.iter() {
+        println!("ループしているぞい: {}", v);
+    }
+}
+
+fn another_fun(x: i32) {
+    println!(
+        "私はmainではないところから呼ばれています。渡されたのは{}",
+        x
+    );
 }
