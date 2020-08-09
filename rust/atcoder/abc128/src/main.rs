2af3850b537823d6f7a23321ded65368be321afd
otatakuma <otatakuma0714@gmail.com>
2020-08-09 12:17:13 +0900

    abc128 c

--- rust/atcoder/abc128/src/main.rs
+++ rust/atcoder/abc128/src/main.rs
@@ -1,22 +1,85 @@
 // -*- coding:utf-8-unix -*-
 
+use proconio::derive_readable;
+use proconio::fastout;
 use proconio::input;
+use std::convert::TryInto;
+use std::io::*;
+use std::str::FromStr;
 
-// ABC086C - Traveling
-// https://atcoder.jp/contests/abs/fasks/arc089_a
+pub fn read<T: FromStr>() -> T {
+    let stdin = stdin();
+    let stdin = stdin.lock();
+    let token: String = stdin
+        .bytes()
+        .map(|c| c.expect("failed to read char") as char)
+        .skip_while(|c| c.is_whitespace())
+        .take_while(|c| !c.is_whitespace())
+        .collect();
+    token.parse().ok().expect("failed to parse token")
+}
 
+//abc128-C
+
+#[fastout]
 fn main() {
-    input! {
-        n: usize,
-        mut plan: [(i32, i32, i32); n],  // Vec<(i32, i32, i32)>
+    //     input! {
+    //         n: u8,
+    //         m: u8,
+    //         sk: [[u8]; n],
+    //         ps: [u8; m]
+    //     }
+
+    //     println!(
+    //         r#"
+    // n: {},
+    // m: {},
+    // sk: {:?},
+    // ps: {:?}
+    //         "#,
+    //         n, m, sk, ps
+    //     );
+
+    let n: usize = read();
+    let m: usize = read();
+    let mut s: Vec<Vec<usize>> = vec![vec![]; m];
+    let mut p: Vec<usize> = vec![];
+
+    for i in 0..m {
+        let k = read();
+        for _ in 0..k {
+            s[i].push(read());
+        }
     }
-    plan.insert(0, (0, 0, 0));
-    let yes = plan.windows(2).all(|w| {
-        let (t0, x0, y0) = w[0];
-        let (t1, x1, y1) = w[1];
-        let time = t1 - t0;
-        let dist = (x1 - x0).abs() + (y1 - y0).abs();
-        dist <= time && time % 2 == dist % 2
-    });
-    println!("{}", if yes { "Yes" } else { "No" });
+
+    for _ in 0..m {
+        p.push(read());
+    }
+    let mut ans = 0;
+
+    let mut sstate = vec![false; n];
+
+    for switch_state in 0..(2u64.pow(n.try_into().unwrap())) {
+        for i in 0..n {
+            sstate[i] = (switch_state >> i) % 2 == 0
+        }
+
+        let mut is_ok = true;
+        for i in 0..m {
+            let mut sum = 0;
+            for s in &s[i] {
+                sum += if sstate[s - 1] { 1 } else { 0 }
+            }
+
+            if sum % 2 != p[i] {
+                is_ok = false;
+            }
+        }
+
+        if is_ok {
+            ans += 1;
+        }
+    }
+
+    println!("{}", ans);
 }
