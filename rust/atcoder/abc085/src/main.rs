51bcafab40088c5794d960444cc9a70686a042ff
otatakuma <otatakuma0714@gmail.com>
2020-08-29 12:56:58 +0900

    rust abc085 failed

--- rust/atcoder/abc085/src/main.rs
+++ rust/atcoder/abc085/src/main.rs
@@ -34,188 +34,53 @@ pub fn read<T: FromStr>() -> T {
     token.parse().ok().expect("failed to parse token")
 }
 
-const can_move: [(i32, i32); 20] = [
-    (-2, -2),
-    (-2, -1),
-    (-2, 0),
-    (-2, 1),
-    (-2, 2),
-    (-1, -2),
-    (-1, -1),
-    (-1, 1),
-    (-1, 2),
-    (0, -2),
-    (0, 2),
-    (1, -2),
-    (1, -1),
-    (1, 1),
-    (1, 2),
-    (2, -2),
-    (2, -1),
-    (2, 0),
-    (2, 1),
-    (2, 2),
-];
-
 lazy_static! {
     static ref H: Mutex<Vec<i32>> = Mutex::default();
     static ref W: Mutex<Vec<i32>> = Mutex::default();
 }
 
-//abc176-D
+//abc085-D
 // #[fastout]
 fn main() {
-    input![
-        h: usize,
-        w: usize,
-        ch: usize,
-        cw: usize,
-        dH: usize,
-        dW: usize,
-        s: [String; h]
-    ];
+    input![n: usize, h: usize, ab: [(usize, usize); n]];
 
-    let ch = ch - 1;
-    let cw = cw - 1;
-    let dH = dH - 1;
-    let dW = dW - 1;
-
-    let mut dp = vec![vec![std::i32::MAX - 2; w]; h];
-    dp[dH][dW] = 0;
-    let mut is_visit = vec![vec![false; w]; h];
-
-    // let ans = std::thread::Builder::new()
-    //     .name("big stack size".into())
-    //     .stack_size(32 * 1024 * 1024) // 32 MBのスタックサイズ
-    //     // .stack_size(1024 * 100) // 32 MBのスタックサイズ
-    //     .spawn(move || {
-    //         // ここで深い再帰を実行
-    //         go(ch, cw, &mut dp, &mut is_visit, h, w, &s, (10000, 10000))
-    //     })
-    //     .unwrap()
-    //     .join()
-    //     .unwrap();
-
-    let ans = go(ch, cw, &mut dp, &mut is_visit, h, w, &s);
-
-    // println!("{:?}", s);
-
-    println!(
-        "{}",
-        if ans >= (std::i32::MAX - 1) as usize {
-            -1
-        } else {
-            ans as i32
-        }
-    );
-}
-
-fn go(
-    hh: usize,
-    ww: usize,
-    dp: &mut Vec<Vec<i32>>,
-    is_visit: &mut Vec<Vec<bool>>,
-    h: usize,
-    w: usize,
-    s: &Vec<String>,
-) -> usize {
-    // eprintln!("{}, {}, {:?}", hh, ww, is_visit);
-
-    if dp[hh][ww] < std::i32::MAX - 2 {
-        return dp[hh][ww] as usize;
+    let mut ab_sorted = Vec::new();
+    for (a, b) in ab.iter() {
+        ab_sorted.push(Reverse((true, *a)));
+        ab_sorted.push(Reverse((false, *b)));
     }
 
-    let mut min_magic = std::i32::MAX - 2;
-    // let mut is_visit = is_visit.clone();
-    is_visit[hh][ww] = true;
+    ab_sorted.sort();
 
-    let walk: [(i32, i32); 4] = [(-1, 0), (0, -1), (0, 1), (1, 0)];
-
-    for (del_h, del_w) in walk.iter() {
-        if hh as i32 + del_h < 0
-            || hh as i32 + del_h >= h as i32
-            || ww as i32 + del_w < 0
-            || ww as i32 + del_w >= w as i32
-        {
-            continue;
-        }
-
-        let new_h = (hh as i32 + del_h) as usize;
-        let new_w = (ww as i32 + del_w) as usize;
-
-        if s[new_h].chars().nth(new_w).unwrap() == '#' || is_visit[new_h][new_w] {
-            // if s[new_h].chars().nth(new_w).unwrap() == '#' || before == (new_w, new_w) {
-            continue;
-        }
-
-        min_magic = min(
-            min_magic,
-            go(
-                (hh as i32 + del_h) as usize,
-                (ww as i32 + del_w) as usize,
-                dp,
-                // &mut is_visit,
-                is_visit,
-                h,
-                w,
-                s,
-            ) as i32,
-        )
+    let mut comsum = Vec::new();
+    comsum.push(Reverse((ab_sorted[0].0).1));
+    for i in 1..(2 * n) {
+        comsum.push(Reverse((ab_sorted[i].0).1 + comsum[i - 1].0));
     }
 
-    for (del_h, del_w) in can_move.iter() {
-        if hh as i32 + del_h < 0
-            || hh as i32 + del_h >= h as i32
-            || ww as i32 + del_w < 0
-            || ww as i32 + del_w >= w as i32
-        {
-            continue;
+    let mut ans = std::i64::MAX;
+    for (a, b) in ab.iter() {
+        let mut mid_ans: i64 = 0;
+        let mut h: i64 = h as i64;
+        let idx = ab_sorted.upper_bound(&Reverse((true, *a)));
+        eprintln!("{}, {:?}", a, ab_sorted);
+        eprintln!("{:?}", comsum);
+        eprintln!("{}", idx);
+
+        mid_ans += idx as i64;
+        h -= comsum[idx - 1].0 as i64;
+
+        eprintln!("rest_h: {}, mid_ans: {}", h, mid_ans);
+        if h > 0 {
+            let a = *a as i64;
+            mid_ans += h / a + if h % a != 0 { 1 } else { 0 };
         }
+        eprintln!("mid_ans: {}", mid_ans);
 
-        let new_h = (hh as i32 + del_h) as usize;
-        let new_w = (ww as i32 + del_w) as usize;
-
-        // eprintln!(
-        //     "{}, {}, {}, {}, {}, {:?}",
-        //     h,
-        //     new_h,
-        //     w,
-        //     new_w,
-        //     ww as i32 + del_w,
-        //     s[new_h].chars().nth(new_w)
-        // );
-        // eprintln!("{}", is_visit[new_h][new_w]);
-        if s[new_h].chars().nth(new_w).unwrap() == '#' || is_visit[new_h][new_w] {
-            // if s[new_h].chars().nth(new_w).unwrap() == '#' || before == (new_w, new_w) {
-            continue;
-        }
-
-        min_magic = min(
-            min_magic,
-            go(
-                (hh as i32 + del_h) as usize,
-                (ww as i32 + del_w) as usize,
-                dp,
-                // &mut is_visit,
-                is_visit,
-                h,
-                w,
-                s,
-            ) as i32
-                + 1,
-        );
+        ans = min(mid_ans, ans);
     }
 
-    dp[hh][ww] = min_magic as i32;
-    // eprintln!("{}, {}, {}", hh, ww, dp[hh][ww].unwrap());
-    is_visit[hh][ww] = false;
-
-    let a = if std::i32::MAX - 2 == min_magic {
-        std::i32::MAX - 1
-    } else {
-        min_magic
-    };
-    return a as usize;
+    println!("{}", ans);
 }
 
 // let mut values = VALUES.lock().unwrap();
